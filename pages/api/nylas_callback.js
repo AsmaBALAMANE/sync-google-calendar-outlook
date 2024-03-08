2820b6d959c47195ca8a1e804eff49240a8e61a4
Ram Bansal <ram.bansal@nylas.com>
2024-03-08 15:55:19 -0500

    migrate from nylas api v2 to v3

--- pages/api/nylas_callback.js
+++ pages/api/nylas_callback.js
@@ -6,68 +6,78 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function handler(req, res) {
-  const client_id = process.env.NYLAS_CLIENT_ID;
-  const client_secret = process.env.NYLAS_CLIENT_SECRET;
+  console.log('req.query.code', req.query.code)
+  const client_id = process.env.NYLAS_CLIENT_ID_V3;
+  // const redirect_uri = `${process.env.NEXT_PUBLIC_VERCEL_URI}/api/nylas_callback`;
+  const client_secret = process.env.NYLAS_CLIENT_SECRET_V3;
+  const bearerToken = process.env.NYLAS_API_KEY;
   const code = req.query.code;
 
-  // Add global variables to temporarily store email and access token
+  // Add global variables to temporarily store email, grantId and bearer token
   let email = '';
-  let accessToken = '';
-  let calendar = '';
+  let grantId = '';
+  let calendar;
 
   // Utility for creating Nylas header for API calls
-  const createHeaders = () => {
-    let headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    headers.append("Authorization", `Bearer ${accessToken}`);
-    return headers;
-  }
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Accept", "application/json");
+  headers.append("Authorization", `Bearer ${bearerToken}`);
+  
+  const raw = JSON.stringify({
+    "code": req.query.code,
+    "client_id": client_id,
+    "client_secret": client_secret,
+    "redirect_uri": redirect_uri,
+    "grant_type": "authorization_code"
+  });
+  
+  const requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: raw,
+    redirect: "follow"
+  };
 
-  fetch(`https://api.nylas.com/oauth/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=authorization_code&code=${code}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  fetch("https://api.us.nylas.com/v3/connect/token", requestOptions)
   .then(response => response.text())
   .then(response => {
     const userData = JSON.parse(response);
+    // console.log('userData', {userData});
     email = userData.email_address;
-    accessToken = userData.access_token;
+    // accessToken = userData.access_token;
+    console.log('userData.grant_id', userData.grant_id)
+    grantId = userData.grant_id;
 
-    return fetch("https://api.nylas.com/calendars", {
+    return fetch(`https://api.us.nylas.com/v3/grants/${grantId}/calendars/primary`, {
       method: 'GET',
-      headers: createHeaders(),
+      headers,
       redirect: 'follow'
     });
   })
   .then(response => response.json())
-  .then(calendars => {
-    const calendar = calendars.filter(({ name }) => 
-    name === email || name === 'Calendar'
-    )[0];
+  .then(response => {
+    console.log({ response })
+    const calendar = response.data
     
-    console.log('calendar selected', calendar);
-    if(!calendar) {
-      console.log('main calendar not found')
-      res.status(200).redirect(process.env.NEXT_PUBLIC_VERCEL_URI)
-    }
+    console.log({ calendar });
 
     // Retrieve calendar events starting now and starting before 7 days
     const now = Math.floor(Date.now()/1000);
     // 604800 is 7 days in seconds
     const future = now + 604800;
 
-    return fetch(`https://api.nylas.com/events?calendar_id=${calendar.id}&starts_after${now}&starts_before${future}`, {
+    return fetch(`https://api.us.nylas.com/v3/grants/${grantId}/events?limit=50&calendar_id=primary&start=${now}&end=${future}`, {
       method: 'GET',
-      headers: createHeaders(),
+      headers,
       redirect: 'follow'
     })
   })
   .then(response => response.json())
   .then(events => {
-    const eventsToStore = events.map(mapEventData);
-    console.log('events', eventsToStore);
+    console.log({events})
+    const eventsToStore = events.data.map(mapEventData);
+    console.log('event', eventsToStore);
 
     return supabase
       .from('calendar_events')
